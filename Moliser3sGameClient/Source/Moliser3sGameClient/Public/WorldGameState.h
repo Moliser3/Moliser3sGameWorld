047d261f2ebfc4369f2b8606fc59ad2aaf8d169f
Moliser3 <moliser3@qq.com>
2026-06-02 17:08:10 +0800

    创建世界各个控制脚本

---
+++ Moliser3sGameClient/Source/Moliser3sGameClient/Public/WorldGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "WorldGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOLISER3SGAMECLIENT_API AWorldGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Moliser3sGameClient/Source/Moliser3sGameClient/Public/WorldPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "WorldPlayerController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class MOLISER3SGAMECLIENT_API AWorldPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+};
