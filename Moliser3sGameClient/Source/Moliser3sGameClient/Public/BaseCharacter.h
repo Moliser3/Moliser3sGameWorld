7002a8e0c37f85f1ba37346125078fe70b20b2db
Moliser3 <moliser3@qq.com>
2026-06-09 17:54:09 +0800

    动画重定向
    角色骨骼动画重定向

--- Moliser3sGameClient/Source/Moliser3sGameClient/Public/BaseCharacter.h
+++ Moliser3sGameClient/Source/Moliser3sGameClient/Public/BaseCharacter.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Character.h"
 #include "BaseCharacter.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class MOLISER3SGAMECLIENT_API ABaseCharacter : public ACharacter
 {
 	GENERATED_BODY()
